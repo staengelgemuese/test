5eaf5da090ceec1d10ad288eee7f6cfc9fdfea0a
Simone Stange <simone_stange@gmx.de>
2016-03-31 18:03:22 +0200

    qtzghfad

--- Auswertungen/fftsdg.m
+++ Auswertungen/fftsdg.m
@@ -1,54 +1,57 @@
-test = dlmread('re200dg2cpd60cl.dat', '',1,0);
-test2 = dlmread('re100dg2cpd60cd.dat', '',1,0);
+test = dlmread('re100dg3cpd60cl.dat', '',1,0);
+%test2 = dlmread('re100dg1cpd40cd.dat', '',1,0);
 t= test(:,1);
 c = test(:,2);
+
+
+a1 = find(test(:,1) >= 90); %filtert alle Werte ab t>= 90 raus
+t1 = test(a1,1);    %Zeit
+cl = test(a1,2);    %Auftrieb
+%cd = test2(a1,2);
+
+figure
 plot(t,c)
+xlabel('Time', 'FontSize', 12)
+ylabel('C_L', 'FontSize', 12)
+hold off
+figure
+plot(t1,cl)
+xlabel('Time', 'FontSize', 12)
+ylabel('C_L', 'FontSize', 12)
+hold off
+figure 
+plot(t1,cd)
+xlabel('Time', 'FontSize', 12)
+ylabel('C_D', 'FontSize', 12)
+hold off
 
-a1 = find(test(:,1) >= 110);
-t1 = test(a1,1);
-cl = test(a1,2);
-cd = test2(a1,2);
-
-Fs = 1;
-T = 1/Fs;            % Sampling frequency           % Sampling period
-L = length(cl)-1;             % Length of signal
-t = (0:L-1)*T;
+T=150; % im Bereich 90-150s -> 60
+Fs = 1/T;
+%T = 1/Fs;           
+L = length(cl)-1;     %Zeitschrittvektor ist eins kürzer als Auftriebsvektor
+t = (0:L)*T;
 Y = fft(cl);
+
 
 P2 = abs(Y/L);
 P1 = P2(1:L/2+1);
 P1(2:end-1) = 2*P1(2:end-1);
-
+[M,I] = max(P1)
+f1=f(I);
 f = Fs*(0:(L/2))/L;
-% plot(f,P1)
-f1 = f(find(P1 == max(P1)));
-dt = 60/L;
+f1 = f(I);
+dt = mean(t1(2:end,1)-t1(1:end-1,1)); %Zeitschritt 
+strouhal = f1/dt*T %bezieht Frequenz auf physikalische Zeit
 
-strouhal = f1/dt
-
-cl_mean=mean(cl)
-cl_max=max(cl)-mean(cl);
-cl_min=min(cl)-mean(cl);
-clabwe = (cl_max-cl_min)/2
-
-
-cd_mean = mean(cd)
-cd_abweichungoben = max(cd)-cd_mean;
-cd_abweichungunten = min(cd)-cd_mean;
-abweichung = (cd_abweichungoben - cd_abweichungunten)/2
-
-
-
-% n = length(cl)
-% dt = mean(t1(2:end,1)-t1(1:end-1,1))
-% fs = 1/dt;
-% [pxx,f] = periodogram(cl-mean(cl),[],[],fs);
-% mean(f)
-% mean(f)*dt
+% cl_mean=mean(cl)
+% cl_max=max(cl)-mean(cl);
+% cl_min=min(cl)-mean(cl);
+% clabwe = (cl_max-cl_min)/2
 % 
-% % plot(t1,cl)
-% plot(f,pxx)
-% 0.1056/dt
-% [ClYD1MP4,ClID1MP4]=max(abs(fft(cl-cl_mean)));
-% ClfreqD1MP4=0:fs/length(ClyD1MP4):Fs/2;
-% Clfreq(1,1)=ClfreqD1MP4(ClID1MP4);
+% 
+% cd_mean = mean(cd)
+% cd_abweichungoben = max(cd)-cd_mean;
+% cd_abweichungunten = min(cd)-cd_mean;
+% abweichung = (cd_abweichungoben - cd_abweichungunten)/2
+
+
